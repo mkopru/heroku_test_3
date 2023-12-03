885ad44609e7dc9aa5f7d1c890ea4662f5acd1b3
mkopru <mmkoprulu@gmail.com>
2023-12-02 18:28:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+# This will store the products in-memory; note that this data will be lost when the app restarts
+products = []
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        product_name = request.form.get('productName')
+        product_link = request.form.get('productLink')
+
+        # Append the new product to the list
+        products.append({'name': product_name, 'link': product_link})
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
