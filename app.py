9951c5ace2658ae53eeba36f97f9e119ac0f836c
mkopru <mmkoprulu@gmail.com>
2023-12-03 15:47:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,90 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change to a random secret key
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    link = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Custom Jinja filter
 def format_datetime(value, format='%m/%d/%Y'):
-    if value is None:
-        return ""
-    # Check if 'value' is already a datetime object
     if isinstance(value, datetime):
         return value.strftime(format)
-    # If 'value' is a string, parse it first
-    return datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f').strftime(format)
-
+    return value
 
 app.jinja_env.filters['datetime'] = format_datetime
 
-def get_db_connection():
-    conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
-    return conn
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+
+        new_user = User(username=username, password_hash=generate_password_hash(password))
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not check_password_hash(user.password_hash, password):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def index():
+    user_id = current_user.id
     conn = get_db_connection()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
 
@@ -32,17 +92,17 @@ def index():
         product_name = request.form.get('productName')
         product_link = request.form.get('productLink')
 
-        cursor.execute("INSERT INTO products (name, link) VALUES (%s, %s)", (product_name, product_link))
+        cursor.execute("INSERT INTO products (name, link, user_id) VALUES (%s, %s, %s)", (product_name, product_link, user_id))
         conn.commit()
 
         return redirect(url_for('index'))
 
-    cursor.execute("SELECT * FROM products")
+    cursor.execute("SELECT * FROM products WHERE user_id = %s", (user_id,))
     products = cursor.fetchall()
     conn.close()
 
     return render_template('index.html', products=products)
 
-
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
